630b696b8edccd494ba017072400ea0127d2be9c
Koen Ekelschot <koen.ekelschot@gmail.com>
2020-12-06 21:20:04 +0100

    Add fail2ban Homeassistant

--- projects/proxy/deploy.sh
+++ projects/proxy/deploy.sh
@@ -9,10 +9,14 @@ fix_dsm() {
 }
 
 echo "Deploy swag"
-ensure_folder ${VOLUMES}/swag
-cp ./projects/proxy/geoip2.conf ${VOLUMES}/swag/nginx/geoip2.conf
-cp ./projects/proxy/nginx.conf ${VOLUMES}/swag/nginx/nginx.conf
-cp ./projects/proxy/homeassistant.proxy.conf ${VOLUMES}/swag/nginx/proxy-confs/homeassistant.subdomain.conf
+ensure_folder ${VOLUMES}/swag/nginx/proxy-confs
+ensure_folder ${VOLUMES}/swag/fail2ban/filter.d
+ensure_folder ${VOLUMES}/homeassistant
+cp ./projects/proxy/nginx/geoip2.conf ${VOLUMES}/swag/nginx/geoip2.conf
+cp ./projects/proxy/nginx/nginx.conf ${VOLUMES}/swag/nginx/nginx.conf
+cp ./projects/proxy/nginx/homeassistant.proxy.conf ${VOLUMES}/swag/nginx/proxy-confs/homeassistant.subdomain.conf
+cp ./projects/proxy/fail2ban/jail.conf ${VOLUMES}/swag/fail2ban/jail.local
+cp ./projects/proxy/fail2ban/homeassistant.filter.conf ${VOLUMES}/swag/fail2ban/filter.d/homeassistant.local
 # Freeing up port 80 on Synology DSM, rewrites port 80 to 8880 and 443 to 8881
 # https://stackoverflow.com/a/55561347
 if [ ! -f /usr/syno/share/nginx/server.mustache.bak ]; then
