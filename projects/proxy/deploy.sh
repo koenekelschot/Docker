34e9a1ec0402c6dbff4c006601ec701dea40fb78
Koen Ekelschot <koen.ekelschot@gmail.com>
2020-12-06 16:49:45 +0100

    Add geo block to proxy

--- projects/proxy/deploy.sh
+++ projects/proxy/deploy.sh
@@ -4,3 +4,6 @@ ensure_folder() {
 
 echo "Deploy swag"
 ensure_folder ${VOLUMES}/swag
+cp ./projects/proxy/geoip2.conf ${VOLUMES}/swag/nginx/geoip2.conf
+cp ./projects/proxy/nginx.conf ${VOLUMES}/swag/nginx/nginx.conf
+cp ./projects/proxy/homeassistant.proxy.conf ${VOLUMES}/swag/nginx/proxy-confs/homeassistant.subdomain.conf
