95568bfed19ceea6f38821541d938ef784bb5c3c
Koen Ekelschot <koen.ekelschot@gmail.com>
2020-12-07 13:50:34 +0100

    Fix fail2ban

--- projects/proxy/deploy.sh
+++ projects/proxy/deploy.sh
@@ -11,11 +11,12 @@ fix_dsm() {
 echo "Deploy swag"
 ensure_folder ${VOLUMES}/swag/nginx/proxy-confs
 ensure_folder ${VOLUMES}/swag/fail2ban/filter.d
-ensure_folder ${VOLUMES}/homeassistant
+ensure_folder ${VOLUMES}/swag/fail2ban/action.d
 cp ./projects/proxy/nginx/geoip2.conf ${VOLUMES}/swag/nginx/geoip2.conf
 cp ./projects/proxy/nginx/nginx.conf ${VOLUMES}/swag/nginx/nginx.conf
 cp ./projects/proxy/nginx/homeassistant.proxy.conf ${VOLUMES}/swag/nginx/proxy-confs/homeassistant.subdomain.conf
 cp ./projects/proxy/fail2ban/jail.conf ${VOLUMES}/swag/fail2ban/jail.local
+cp ./projects/proxy/fail2ban/iptables-common-override.conf ${VOLUMES}/swag/fail2ban/action.d/iptables-common.local
 cp ./projects/proxy/fail2ban/homeassistant.filter.conf ${VOLUMES}/swag/fail2ban/filter.d/homeassistant.local
 # Freeing up port 80 on Synology DSM, rewrites port 80 to 8880 and 443 to 8881
 # https://stackoverflow.com/a/55561347
@@ -24,4 +25,7 @@ if [ ! -f /usr/syno/share/nginx/server.mustache.bak ]; then
     fix_dsm /usr/syno/share/nginx/DSM.mustache
     fix_dsm /usr/syno/share/nginx/WWWService.mustache
     /usr/syno/sbin/synoservicecfg --restart nginx
+fi
+if [ "$( /usr/local/bin/docker container inspect -f '{{.State.Status}}' swag )" == "running" ]; then
+    /usr/local/bin/docker restart swag
 fi
