d70f054e8624cab5564995fbbb9624f1a72799b0
Koen Ekelschot <koen.ekelschot@gmail.com>
2020-12-09 22:51:25 +0100

    Secure nginx

--- projects/nginx/deploy.sh
+++ projects/nginx/deploy.sh
@@ -9,19 +9,22 @@ fix_dsm() {
 }
 
 echo "Deploy nginx"
-ensure_folder ${VOLUMES}/nginx/conf.d/sites
+#ensure_folder ${VOLUMES}/nginx/conf
 ensure_folder ${VOLUMES}/nginx/logs
+ensure_folder ${VOLUMES}/nginx/servers
 touch ${VOLUMES}/nginx/logs/default-access.log
 touch ${VOLUMES}/nginx/logs/default-error.log
 touch ${VOLUMES}/nginx/logs/homeassistant-access.log
 touch ${VOLUMES}/nginx/logs/homeassistant-acme-challenge.log
 touch ${VOLUMES}/nginx/logs/homeassistant-error.log
-cp ./projects/nginx/nginx.conf ${VOLUMES}/nginx/conf.d/nginx.conf
-cp ./projects/nginx/geoip2.conf ${VOLUMES}/nginx/conf.d/geoip2.conf
-cp ./projects/nginx/proxy.conf ${VOLUMES}/nginx/conf.d/proxy.conf
-cp ./projects/nginx/ssl.conf ${VOLUMES}/nginx/conf.d/ssl.conf
-cp ./projects/nginx/sites/default.conf ${VOLUMES}/nginx/conf.d/sites/default.conf
-cp ./projects/nginx/sites/homeassistant.conf ${VOLUMES}/nginx/conf.d/sites/homeassistant.conf
+touch ${VOLUMES}/nginx/logs/homeassistant-ssl-access.log
+touch ${VOLUMES}/nginx/logs/homeassistant-ssl-error.log
+cp ./projects/nginx/nginx.conf ${VOLUMES}/nginx/nginx.conf
+cp ./projects/nginx/servers/* ${VOLUMES}/nginx/servers/
+# cp ./projects/nginx/servers/proxy.conf ${VOLUMES}/nginx/servers/proxy.conf
+# cp ./projects/nginx/servers/ssl.conf ${VOLUMES}/nginx/servers/ssl.conf
+# cp ./projects/nginx/servers/default.conf ${VOLUMES}/nginx/servers/default.conf
+# cp ./projects/nginx/servers/homeassistant.conf ${VOLUMES}/nginx/servers/homeassistant.conf
 
 # Freeing up port 80 on Synology DSM, rewrites port 80 to 8880 and 443 to 8881
 # https://stackoverflow.com/a/55561347
@@ -31,8 +34,8 @@ if [ ! -f /usr/syno/share/nginx/server.mustache.bak ]; then
     fix_dsm /usr/syno/share/nginx/WWWService.mustache
     /usr/syno/sbin/synoservicecfg --restart nginx
 fi
-if [ ! -f ${VOLUMES}/nginx/conf.d/dhparams.pem ]; then
-    /bin/openssl dhparam -dsaparam -out ${VOLUMES}/nginx/conf.d/dhparams.pem 4096
+if [ ! -f ${VOLUMES}/nginx/dhparams.pem ]; then
+    /bin/openssl dhparam -dsaparam -out ${VOLUMES}/nginx/dhparams.pem 4096
 fi
 if [ "$( /usr/local/bin/docker container inspect -f '{{.State.Status}}' nginx )" == "running" ]; then
     /usr/local/bin/docker restart nginx
