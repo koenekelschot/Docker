606028614d856161c95095e713889b0be56b095e
Koen Ekelschot <koen.ekelschot@gmail.com>
2020-12-11 17:03:26 +0100

    Reconfigure fail2ban

--- projects/fail2ban/deploy.sh
+++ projects/fail2ban/deploy.sh
@@ -2,16 +2,23 @@ ensure_folder() {
     test -d "$1" || mkdir -p "$1"
 }
 
+copy_config() {
+    cp ./projects/fail2ban/$1.conf ${VOLUMES}/fail2ban/$1
+}
+
 echo "Deploy fail2ban"
 ensure_folder ${VOLUMES}/fail2ban
 ensure_folder ${VOLUMES}/fail2ban/action.d
 ensure_folder ${VOLUMES}/fail2ban/filter.d
 ensure_folder ${VOLUMES}/fail2ban/jail.d
-cp ./projects/fail2ban/action.d/iptables-common.local.conf ${VOLUMES}/fail2ban/action.d/iptables-common.local
-cp ./projects/fail2ban/filter.d/homeassistant.local.conf ${VOLUMES}/fail2ban/filter.d/homeassistant.local
-cp ./projects/fail2ban/filter.d/nginx-badbots.local.conf ${VOLUMES}/fail2ban/filter.d/nginx-badbots.local
-cp ./projects/fail2ban/filter.d/nginx-deny.local.conf ${VOLUMES}/fail2ban/filter.d/nginx-deny.local
-cp ./projects/fail2ban/jail.d/jail.local.conf ${VOLUMES}/fail2ban/jail.d/jail.local
+copy_config action.d/iptables-common.local
+copy_config filter.d/nginx-authfail.local
+copy_config filter.d/nginx-badbots.local
+copy_config filter.d/nginx-deny.local
+copy_config filter.d/nginx-geoblock.local
+copy_config filter.d/nginx-notme.local
+copy_config filter.d/nginx-untagged.local
+copy_config jail.d/jail.local
 if [ "$( /usr/local/bin/docker container inspect -f '{{.State.Status}}' fail2ban )" == "running" ]; then
     /usr/local/bin/docker restart fail2ban
 fi
