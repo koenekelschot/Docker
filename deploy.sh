5096d097178072ec8bba4cff1ebeb70029e2eb2e
Koen Ekelschot <koen.ekelschot@gmail.com>
2021-02-01 22:17:37 +0100

    Add Dockerfiles to repo

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 find "./projects" -mindepth 1 -name "deploy.sh" -exec sh {} \;
-/usr/local/bin/docker-compose up -d --remove-orphans
+/usr/local/bin/docker-compose up -d --build --remove-orphans
