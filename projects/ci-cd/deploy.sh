6906799138ffcd6f6bf647cf253d5aea03492313
Koen Ekelschot <koen.ekelschot@gmail.com>
2021-08-14 13:52:14 +0200

    remove redis

--- projects/ci-cd/deploy.sh
+++ projects/ci-cd/deploy.sh
@@ -2,10 +2,6 @@ ensure_folder() {
     test -d "$1" || mkdir -p "$1"
 }
 
-echo "Deploy redis"
-ensure_folder ${VOLUMES}/redis
-chown -R 999:999 ${VOLUMES}/redis
-
 echo "Deploy gitlab"
 ensure_folder ${VOLUMES}/gitlab-runner
 cp ./projects/ci-cd/config/gitlab-runner.toml ${VOLUMES}/gitlab-runner/config.toml
