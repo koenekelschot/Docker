3e43507d9731413bfb0984bf88f1c2ab1d16b341
Koen Ekelschot <koen.ekelschot@gmail.com>
2022-09-18 14:07:16 +0200

    fixes gitlab

--- cron/backup-gitlab.sh
+++ cron/backup-gitlab.sh
@@ -1,2 +1,2 @@
-# runs as root every day at 04:00
-./volume1/docker/volumes/gitlab/gitlab-restore-backup.sh
+# runs as root every day at 03:00
+/volume1/docker/volumes/gitlab/gitlab-restore-backup.sh
