f978804e47cf9b00b8de562740bc7aa9f1d9070b
Koen Ekelschot <koen.ekelschot@gmail.com>
2022-09-27 21:16:27 +0200

    fix waf issues

--- cron/backup-gitlab.sh
+++ cron/backup-gitlab.sh
@@ -1,2 +1,2 @@
 # runs as root every day at 03:00
-/volume1/docker/volumes/gitlab/gitlab-restore-backup.sh
+/volume1/docker/volumes/gitlab/gitlab-create-backup.sh
--- projects/traefik/deploy.sh
+++ projects/traefik/deploy.sh
@@ -34,3 +34,6 @@ fi
 if [ "$( /usr/local/bin/docker container inspect -f '{{.State.Status}}' traefik )" == "running" ]; then
     /usr/local/bin/docker restart traefik
 fi
+if [ "$( /usr/local/bin/docker container inspect -f '{{.State.Status}}' waf )" == "running" ]; then
+    /usr/local/bin/docker restart waf
+fi
