a45e18e304a0e06a6003e82b7fe010944a1c4872
Koen Ekelschot <koen.ekelschot@gmail.com>
2022-09-29 15:53:36 +0200

    improve gitlab backup

--- cron/backup-gitlab.sh
+++ cron/backup-gitlab.sh
@@ -1,2 +1,7 @@
 # runs as root every day at 03:00
-/volume1/docker/volumes/gitlab/gitlab-create-backup.sh
+. /volume1/docker/cron/log.sh
+
+#/volume1/docker/volumes/gitlab/gitlab-create-backup.sh
+log "Starting backup Gitlab"
+log "$(/volume1/docker/volumes/gitlab/gitlab-create-backup.sh)"
+log "Finished backup Gitlab"
--- projects/ci-cd/gitlab-create-backup.sh
+++ projects/ci-cd/gitlab-create-backup.sh
@@ -2,5 +2,5 @@
 docker exec -it gitlab gitlab-ctl stop puma
 docker exec -it gitlab gitlab-ctl stop sidekiq
 docker exec -it gitlab gitlab-backup create
-docker exec -it gitlab gitlab-ctl start sidekiq
-docker exec -it gitlab gitlab-ctl start puma
+docker exec -it gitlab update-permissions >/dev/null 2>&1
+docker restart gitlab >/dev/null 2>&1
