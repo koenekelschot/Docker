41cb81b6b46a69813f0310dc1a5df6c09cddc98b
Koen Ekelschot <koen.ekelschot@gmail.com>
2022-09-30 12:26:42 +0200

    Further improve Gitlab backups

--- cron/backup-gitlab.sh
+++ cron/backup-gitlab.sh
@@ -1,7 +1,29 @@
 # runs as root every day at 03:00
 . /volume1/docker/cron/log.sh
 
-#/volume1/docker/volumes/gitlab/gitlab-create-backup.sh
+restart_gitlab() {
+    log "Restarting"
+    /usr/local/bin/docker exec gitlab update-permissions >/dev/null 2>&1
+    /usr/local/bin/docker restart gitlab >/dev/null 2>&1
+}
+
+create_backup() {
+    log "Creating backup"
+    /usr/local/bin/docker exec gitlab gitlab-backup create
+}
+
 log "Starting backup Gitlab"
-log "$(/volume1/docker/volumes/gitlab/gitlab-create-backup.sh)"
+/usr/local/bin/docker exec gitlab gitlab-ctl stop puma
+/usr/local/bin/docker exec gitlab gitlab-ctl stop sidekiq
+output=$(create_backup)
+if [[ $output == *"Rails Error"* ]]; then
+    log "Gitlab (permission) error, restarting first"
+    restart_gitlab
+    log "Waiting until container is started"
+    until [ "`/usr/local/bin/docker inspect -f {{.State.Running}} gitlab`" == "true" ]; do
+        sleep 0.1;
+    done;
+    create_backup
+fi;
+restart_gitlab
 log "Finished backup Gitlab"
--- projects/ci-cd/gitlab-create-backup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-docker exec -it gitlab gitlab-ctl stop puma
-docker exec -it gitlab gitlab-ctl stop sidekiq
-docker exec -it gitlab gitlab-backup create
-docker exec -it gitlab update-permissions >/dev/null 2>&1
-docker restart gitlab >/dev/null 2>&1
