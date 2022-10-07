b85f2a1bdfb62f0734ceac4fac96c4e8802be61e
Koen Ekelschot <koen.ekelschot@gmail.com>
2022-10-07 18:45:16 +0200

    change homeassistant config

--- projects/hass/deploy.sh
+++ projects/hass/deploy.sh
@@ -5,4 +5,9 @@ ensure_folder() {
 echo "Deploy homeassistant"
 ensure_folder ${VOLUMES}/deconz
 ensure_folder ${VOLUMES}/esphome
-ensure_folder ${VOLUMES}/homeassistant
+ensure_folder ${VOLUMES}/homeassistant/config
+ensure_folder ${VOLUMES}/homeassistant/data
+ensure_folder ${VOLUMES}/homeassistant/logs
+touch ${VOLUMES}/homeassistant/logs/home-assistant.log
+touch ${VOLUMES}/homeassistant/logs/home-assistant.log.1
+touch ${VOLUMES}/homeassistant/logs/home-assistant.log.fault
