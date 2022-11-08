330b2ff024deed9f6674ae05f09e039206a21cb0
Koen Ekelschot <koen.ekelschot@gmail.com>
2022-11-08 21:55:34 +0100

    improve scripts

--- build.py
+++ build.py
@@ -4,21 +4,37 @@ import json
 import subprocess
 from jinja2 import FileSystemLoader, Environment
 
-def get_ssh_path(path):
+def get_ssh_path(path: str) -> str:
     user = os.getenv('SSH_USER')
     host = os.getenv('SSH_HOST')
     folder = os.getenv('SSH_FOLDER_DOCKER')
     return user + '@' + host + ':' + folder + '/' + path
 
-def download_remote_file(path):
+def download_remote_file(path: str) -> None:
     print("Downloading " + path)
     subprocess.run(["rsync", "-athv", get_ssh_path(path), path])
 
-def get_version_from_file(path):
+def get_version_from_file(path: str) -> str:
     with open(path, "r") as file:
         return file.read()
 
-def import_env():
+def merge_files(source_path: str, merge_paths: list[str]) -> None:
+    with open(source_path, "r") as source_file:
+        lines = source_file.readlines()
+    for path in merge_paths:
+        with open(path, "r") as merge_file:
+            lines.append(os.linesep)
+            lines.append("# original file: " + path)
+            for line in merge_file.readlines():
+                lines.append(line)
+    with open(source_path, "w") as target_file:
+        target_file.writelines(lines)
+
+def remove_files(files: list[str]) -> None:
+    for file in files:
+        os.remove(file)
+
+def import_env() -> dict:
     print("Import env variables")
     download_remote_file("env.json")
     download_remote_file(".HA_VERSION")
@@ -33,7 +49,7 @@ def import_env():
     os.remove(".ESPHOME_VERSION")
     return data
 
-def transform_deploy_scripts(variables):
+def transform_deploy_scripts(variables: dict) -> None:
     for source_path in glob.glob("**/deploy.jinja2.sh", recursive=True):
         print("Transforming: " + source_path)
         target_path = source_path.replace(".jinja2.", ".")
@@ -43,19 +59,12 @@ def transform_deploy_scripts(variables):
             target_file.write(template.render(variables))
         os.remove(source_path)
 
-def combine_deploy_scripts():
-    with open("deploy.sh", "r") as source_file:
-        lines = source_file.readlines()
-    for path in glob.glob("projects/**/deploy.sh", recursive=True):
-        with open(path, "r") as merge_file:
-            lines.append(os.linesep)
-            for line in merge_file.readlines():
-                lines.append(line)
-        os.remove(path)
-    with open("deploy.sh", "w") as target_file:
-        target_file.writelines(lines)
+def merge_deploy_scripts() -> None:
+    to_merge = glob.glob("projects/**/deploy.sh", recursive=True)
+    merge_files("deploy.sh", to_merge)
+    remove_files(to_merge)
 
-def transform_jinja_files(variables):
+def transform_jinja_files(variables: dict) -> None:
     for source_path in glob.glob("projects/**/*.jinja2.*", recursive=True):
         print("Transforming: " + source_path)
         target_path = source_path.replace(".jinja2.", ".")
@@ -65,35 +74,21 @@ def transform_jinja_files(variables):
             target_file.write(template.render(variables))
         os.remove(source_path)
 
-def combine_docker_compose():
-    with open("docker-compose.yml", "r") as source_file:
-        lines = source_file.readlines()
-    for path in glob.glob("projects/**/*.yml", recursive=False):
-        with open(path, "r") as merge_file:
-            lines.append(os.linesep)
-            for line in merge_file.readlines():
-                lines.append(line)
-        os.remove(path)
-    with open("docker-compose.yml", "w") as target_file:
-        target_file.writelines(lines)
+def merge_docker_compose() -> None:
+    to_merge = glob.glob("projects/**/*.yml", recursive=False)
+    merge_files("docker-compose.yml", to_merge)
+    remove_files(to_merge)
 
-def combine_post_deploy_scripts():
-    with open("post-deploy.sh", "r") as source_file:
-        lines = source_file.readlines()
-    for path in glob.glob("projects/**/post-deploy.sh", recursive=True):
-        with open(path, "r") as merge_file:
-            lines.append(os.linesep)
-            for line in merge_file.readlines():
-                lines.append(line)
-        os.remove(path)
-    with open("post-deploy.sh", "w") as target_file:
-        target_file.writelines(lines)
+def merge_post_deploy_scripts() -> None:
+    to_merge = glob.glob("projects/**/post-deploy.sh", recursive=False)
+    merge_files("post-deploy.sh", to_merge)
+    remove_files(to_merge)
 
 env_json = import_env()
 
 transform_deploy_scripts(env_json)
-combine_deploy_scripts()
-combine_post_deploy_scripts()
+merge_deploy_scripts()
+merge_post_deploy_scripts()
 
 transform_jinja_files(env_json)
-combine_docker_compose()
+merge_docker_compose()
