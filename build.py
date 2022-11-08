0049f0b8601c0c2519e273edb023775fa59d1898
Koen Ekelschot <koen.ekelschot@gmail.com>
2022-11-09 00:20:49 +0100

    fix python linting errors

--- build.py
+++ build.py
@@ -1,6 +1,6 @@
+import json
 import glob
 import os
-import json
 import subprocess
 from jinja2 import Template
 
@@ -12,32 +12,32 @@ def get_ssh_path(path: str) -> str:
 
 def download_remote_file(path: str) -> None:
     print("Downloading " + path)
-    subprocess.run(["rsync", "-athv", get_ssh_path(path), path])
+    subprocess.run(["rsync", "-athv", get_ssh_path(path), path], check=True)
 
 def get_version_from_file(path: str) -> str:
-    with open(path, "r") as file:
+    with open(path, "r", encoding="utf-8") as file:
         return file.read()
 
 def merge_files(source_path: str, merge_paths: list[str], prefix_merged_lines: str = "") -> None:
-    with open(source_path, "r") as source_file:
+    with open(source_path, "r", encoding="utf-8") as source_file:
         lines = source_file.readlines()
     for path in merge_paths:
-        with open(path, "r") as merge_file:
+        with open(path, "r", encoding="utf-8") as merge_file:
             lines.append(prefix_merged_lines + "# original file: " + path)
             lines.append(os.linesep)
             for line in merge_file.readlines():
                 lines.append(prefix_merged_lines + line)
-    with open(source_path, "w") as target_file:
+    with open(source_path, "w", encoding="utf-8") as target_file:
         target_file.writelines(lines)
 
 def transform_files(files: list[str], variables: dict) -> None:
     for source_file in files:
         print("Transforming: " + source_file)
         target_path = source_file.replace(".jinja2.", ".")
-        with open(source_file, "r") as file:
+        with open(source_file, "r", encoding="utf-8") as file:
             data = file.read()
         template = Template(data, keep_trailing_newline=True)
-        with open(target_path, "w") as target_file:
+        with open(target_path, "w", encoding="utf-8") as target_file:
             target_file.write(template.render(variables))
 
 def remove_files(files: list[str]) -> None:
@@ -49,7 +49,7 @@ def import_env() -> dict:
     download_remote_file("env.json")
     download_remote_file(".HA_VERSION")
     download_remote_file(".ESPHOME_VERSION")
-    with open("env.json", "r") as input_file:
+    with open("env.json", "r", encoding="utf-8") as input_file:
         data = json.load(input_file)
     data["hass_version"] = get_version_from_file(".HA_VERSION")
     data["esphome_version"] = get_version_from_file(".ESPHOME_VERSION")
