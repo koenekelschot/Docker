f055e299e1bccda41aa335ceb16a3a906f6e6ae9
Koen Ekelschot <koen.ekelschot@gmail.com>
2022-11-09 22:20:38 +0100

    shellcheck projects

--- projects/ci-cd/gitlab-restore-backup.sh
+++ projects/ci-cd/gitlab-restore-backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Please enter the backup to restore (omit '_gitlab_backup.tar'):"
-read backup
+read -r backup
 docker exec -it gitlab gitlab-ctl stop puma
 docker exec -it gitlab gitlab-ctl stop sidekiq
-docker exec -it gitlab gitlab-backup restore BACKUP=$backup
+docker exec -it gitlab gitlab-backup restore BACKUP="$backup"
 docker restart gitlab
