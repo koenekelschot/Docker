bc2d209a34d8ee31354407269ccf55ffc864b067
Koen Ekelschot <koen.ekelschot@gmail.com>
2022-11-09 21:40:28 +0100

    rewrite deploy

--- deploy.jinja2.sh
+++ deploy.jinja2.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#/usr/local/bin/docker-compose up -d --build --remove-orphans
-
 ensure_folder_exists() {
     test -d "$1" || mkdir -p "$1"
 }
@@ -17,3 +15,8 @@ copy_file() {
 chmod 755 -R /volume1/homes/{{ ssh_user }}
 chmod 700 -R /volume1/homes/{{ ssh_user }}/.ssh
 chmod 600 /volume1/homes/{{ ssh_user }}/.ssh/authorized_keys
+
+if [ ! -f /etc/sudoers.bak ]; then
+    copy_file /etc/sudoers /etc/sudoers.bak
+    echo "{{ ssh_user }} ALL=(ALL:ALL) NOPASSWD:ALL" >> /etc/sudoers
+fi
--- post-deploy.sh
+++ post-deploy.sh
@@ -1,13 +1,9 @@
+#!/bin/bash
+
 empty_folder() {
-    # todo fixen voor lokale aanroep
-    echo "Cleaning folder $SSH_FOLDER_DOCKER/$1"
-    sshpass -e ssh -o StrictHostKeyChecking=no $SSH_USER@$SSH_HOST "test -d $SSH_FOLDER_DOCKER/$1 && rm -r -v $SSH_FOLDER_DOCKER/$1"
+    test -d "$SSH_FOLDER_DOCKER/$1" && rm -r -v "${SSH_FOLDER_DOCKER:?}/$1"
 }
 
 /usr/local/bin/docker-compose up -d --build --remove-orphans
 
 empty_folder "projects"
-
-# alle post-deploys samenvoegen?
-# of 
-# container restarts in gitlab-ci opnemen?
