7ac362c0bbf720dce52a87daaa1eee8040c67c6a
Koen Ekelschot <koen.ekelschot@gmail.com>
2022-11-13 10:54:52 +0100

    status websites

--- projects/websites/deploy.jinja2.sh
+++ projects/websites/deploy.jinja2.sh
@@ -1,9 +1,14 @@
 echo "Deploy websites"
+ensure_folder_exists {{ global.docker_volume }}/websites/php-fpm
+ensure_folder_exists {{ global.docker_volume }}/websites/php-status/config
+ensure_folder_exists {{ global.docker_volume }}/websites/php-status/logs
 ensure_folder_exists {{ global.docker_volume }}/websites/{{ websites.domain_koen }}/config
 ensure_folder_exists {{ global.docker_volume }}/websites/{{ websites.domain_koen }}/logs
 ensure_folder_exists {{ global.docker_volume }}/websites/{{ websites.domain_koen }}/public
 ensure_folder_exists {{ global.docker_volume }}/websites/{{ websites.domain_paul }}/config
 ensure_folder_exists {{ global.docker_volume }}/websites/{{ websites.domain_paul }}/logs
 ensure_folder_exists {{ global.docker_volume }}/websites/{{ websites.domain_paul }}/public
+copy_file ./projects/websites/config/php-fpm.conf {{ global.docker_volume }}/websites/php-fpm/zz-docker.conf
+copy_file ./projects/websites/config/php-status.conf {{ global.docker_volume }}/websites/php-status/config/default.conf
 copy_file ./projects/websites/config/koen.conf {{ global.docker_volume }}/websites/{{ websites.domain_koen }}/config/default.conf
 copy_file ./projects/websites/config/paul.conf {{ global.docker_volume }}/websites/{{ websites.domain_paul }}/config/default.conf
--- projects/websites/post-deploy.sh
+++ projects/websites/post-deploy.sh
@@ -5,3 +5,9 @@ fi
 if [ "$( /usr/local/bin/docker container inspect -f '{{.State.Status}}' website-paul )" == "running" ]; then
     /usr/local/bin/docker restart website-paul
 fi
+if [ "$( /usr/local/bin/docker container inspect -f '{{.State.Status}}' php-fpm )" == "running" ]; then
+    /usr/local/bin/docker restart php-fpm
+fi
+if [ "$( /usr/local/bin/docker container inspect -f '{{.State.Status}}' php-status )" == "running" ]; then
+    /usr/local/bin/docker restart php-status
+fi
